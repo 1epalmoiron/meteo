f5e914065e7f6ec0887c8048117fb741e79e260d
Charitakis Yannis <git@ych.gr>
2018-03-23 08:17:56 +0200

    Initial commit

---
+++ meteo.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+#-*- coding: UTF-8 -*-
+#!/usr/bin/env python
+
+#enable debugging
+
+import mysql.connector
+cnx = mysql.connector.connect(user="meteo17",
+	password="meteo17",
+	host="localhost",
+	database="gp_2017_2018")
+my_cursor = cnx.cursor(named_tuple=True)
+s1="select * From meteorologikos Order by id DESC LIMIT 1;"
+my_cursor.execute(s1)
+
+print "Content-type: text/html\n\n"
+
+print "<html>"
+print"<head>"
+print"<meta charset='UTF-8'>"
+print"<link href='meteo.css' rel='stylesheet'/>"
+print"<style>"
+print" table,th,td {border:2px solid black; border-collapse: collapse;}</style>"
+print"</head>"
+print"<body id='bd'>"
+print"<h1 align='center'><mark>1<sup>o</sup> Επάλ Μοιρών</mark></h1>"
+print "<table id='img'>"
+
+print"<tr id='names'>"
+print"<th><img src='https://www.imageupload.co.uk/images/2018/03/16/MoonRender.png'"
+print"style='width:50; height:50'></th>"
+print"<th>Ημερο-<br>μηνία</th>"
+print"<th>Ώρα</th>"
+print"<th>Θερμο-<br>κρασία</th>"
+print"<th>Υγρασία</th>"
+print"<th>Βαρομετρίκη Πίεση</th>"
+print"<th>Κατεύθηνση Ανέμου</th>"
+print"<th>Ταχύτητα Ανέμου</th>"
+print"<th>Μέγιστη Ταχύτητα Ανέμου</th>"
+print"<th>Χιλιοστά Βροχής</th>"
+print"<th>Ραδιε-<br>νέργια</th>"
+print"<th>Ακτινο-<br>βολία</th>"
+print"<th>EVPT</th>"
+print"<th>Μπαταρία</th>"
+print"<th>Ψύχρα</th>"
+print"</tr>"
+
+for i in my_cursor:
+	print "<tr>"
+	u=float(i.wind_speed) * 1.609344 #metatropi apo m/h se km/h
+	B=float(0.48*u)**(2/3)
+	b=float(i.BARO) * 25.4
+	tx=float(i.kles)* 1.609344
+	BR=float(0.48*tx)**(2/3)
+	y=float(i.insects_of_rain) * 25.4
+	t=float(i.temp)
+	t=(t-32)* 5/9
+	d=i.date.split(" ")
+	n=d[1].index(".")
+	print "<td align='center'>", i.day_night, "</td>"
+	print "<td align='center'>", d[0], "</td>"
+	print "<td align='center'>", d[1][0:n-3], "</td>"
+	print "<td align='center'>",str(round(t,2)),"oC</td>"
+	print "<td align='center'>", i.humidity, "%</td>"
+	print "<td align='center'>", b, "mmHg</td>"
+	print "<td align='center'>", i.wdir, "μοίρες</td>"
+	print "<td align='center'>", round(u),"km/h  ή ",round(B),"B</td>"
+	print "<td align='center'>", round(tx),"km/h ή ",round(BR), "B</td>"
+	print "<td align='center'>", y, "mm</td>"
+	print "<td align='center'>",i.SRAD,"</td>"
+	print "<td align='center'>",i.UV,"</td>"
+	print "<td align='center'>",i.EVPT,"</td>"
+	print "<td align='center'>",i.BATT,"</td>"
+	print "<td align='center'>",i.CHILL,"</td>"
+        print "</tr>"
+
+if t<14:
+	print "<body style='background-color:#3bc8ef;'>"
+elif t<22:
+	print "<body style='background-color:#59e01f;'>"
+elif t<30:
+	print "<body style='background-color:#f99a1d;'>"
+else:
+	print "<body style='background-color:#f91d1d;'>"
+print "</table>"
+
+print " <br><br><a href='meteo_all.py'><font size='5'><strong>Ιστορικό Δεδομένων</strong></font></a>"
+
+print "</body>"
+print "</html>"
+
+
+
+cnx.close()
+
+
---
+++ meteoepalmoiron.php
@@ -0,0 +1,269 @@
+<html>
+<head>
+	<link href="meteo.ccs" rel="stylesheet" />
+<meta charset="UTF-8">
+<title>Μετέο Μεσσαράς</title>
+<style>
+ table, th, td, tr{
+	border: 1px solid #232323;
+	border-collapse: collapse;
+	}
+	th, td {
+		padding: 5px;
+		text-align: left;
+	}
+	body {
+		background-image: url('https://i.igmur.com/kXiS1eZ.jpg')
+	}
+</style>
+</head>
+<body id="bd">
+<h1 align='center'><strong>ΕΠΑΛ Μοιρων</strong></h1>
+<h2 align='center'>Δεδομένα Μετεωρολογικού</h2>
+<?php
+$servername = "localhost";
+$username = "meteo17";
+$password = "meteo17";
+$dbname = "gp_2017_2018";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+$kevtherm=0;
+$aoureltherm=0;
+$xristinatherm=0;
+$boutsitherm=0;
+$papadaktherm=0;
+$nikostherm=0;
+$kapsalistherm=0;
+$giorgostherm=0;
+$doulgetherm=0;
+$anastherm=0;
+$dionistherm=0;
+$xristtherm=0;
+$papadtherm=0;
+$kapsaltherm=0;
+$sql = "SELECT * FROM meteorologikos ORDER BY id DESC LIMIT 1";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+	$kevtherm=$row["day_night"];
+	$aoureltherm=$row["date"];
+	$xristinatherm=$row["hour"];
+	$boutsitherm=$row["temp"];
+	$papadaktherm=$row["humidity"];
+	$nikostherm=$row["BARO"];
+	$kapsalistherm=$row["wdir"];
+	$giorgostherm=$row["wind_speed"];
+	$doulgetherm=$row["insects_of_rain"];
+    $anastherm=$row["SRAD"];
+    $dionistherm=$row["UV"];
+    $xristtherm=$row["EVPT"];
+    $papadtherm=$row["BATT"];  
+    $kapsaltherm=$row["CHILL"];   
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+?>
+
+	<table id="img">
+<?php
+            $boutsitherm = floatval ($boutsitherm);
+            $boutsitherm = ($boutsitherm - 32) * 5/9;
+            $boutsitherm = intval ($boutsitherm);
+        ?>
+    <tr>        
+        <?php
+            if ($boutsitherm > 18 ){
+                echo '<th><img src="https://i.imgur.com/o121blp.png" style="width:50px; height:50"></th>';
+            }
+        ?>
+        <?php
+        /*
+        <th><img src="https://i.imgur.com/0yGO1EZ.png" style="width:50px; height:50"></th>
+ 		
+        <th><img src="https://i.imgur.com/pcRwVTJ.png" style="width:50px; height:50"></th>
+                
+        <th><img src="https://i.imgur.com/s5NCaj9.png" style="width:50px; height:50"></th>
+		          */?>
+        <?php
+            if ($boutsitherm <= 18 and $boutsitherm >=13 ){
+        echo    '<th><img src="https://i.imgur.com/81guYEI.png" style="width:50px; height:50"></th>';
+            }
+		?>
+         <?php
+            if ($boutsitherm < 0){
+        echo    '<th><img src="https://i.imgur.com/YPvFopV.png" style="width:50px; height:50"></th>';
+            }
+        ?>
+        <?php
+            if ($boutsitherm <= 12 and $boutsitherm >=1 ){
+        echo '<th><img src="https://i.imgur.com/PaSe2ba.png" style="width:50px; height:50"></th>';
+            }
+		?>
+        <th><img src = "https://www.imageupload.co.uk/images/2018/03/16/stopwatch.png" style="width:50px; height:50"></th>
+        <th><img src="https://i.imgur.com/o121blp.png" style="width:50px; height:50"></th>
+        <th><img src="https://i.imgur.com/SXMRDm9.png" style="width:50px; height:50"></th>
+        <th><img src="https://preview.ibb.co/dStL4c/humidity.png"
+                 style="width:50px; height:50"></th>
+        <th><img src="https://image.ibb.co/jku6Sx/8762_200.png" style="width:50px; height:50"></th>
+        <th><img src="https://image.ibb.co/bQZ7Hx/519580_076_Location_Arrow_512.png"
+                 style="width:50px; height:50"></th>
+        <th><img src="https://i.imgur.com/WRJywFJ.png" style="width:50px; height:50"></th>
+        
+
+       
+	</tr>
+ 
+        
+        
+        
+	<tr id="names">
+        <?php
+            if ($boutsitherm > 18 ){
+        echo    '<td>Ηλιοφάνεια</td>';
+            }
+        ?>
+        
+        <?php
+            if ($boutsitherm <= 18 and $boutsitherm >=13 ){
+        echo '<td>Ασθενής Βροχή</td>';
+            }
+        ?>
+        
+		<?php
+            if ($boutsitherm <= 12 and $boutsitherm >=1 ){
+        echo '<td>Βροχερός</td>';
+        }
+        ?>
+        
+		<?php
+            if ($boutsitherm < 0){
+        echo '<td>Χιονώδης</td>';
+        }
+        ?>
+		<td>Ώρες λειτουργίας</td>
+        <td>Ημερα/Νυχτα</td>
+        <td>Θερμοκρασία</td>
+        <td>Υγρασία</td>
+        <td>Βαρομετρική Πίεση</td>
+        <td>Κατεύθηνση Ανέμου</td>
+        <td>Ταχτητα Ανεμου</td>
+    
+	</tr>
+        
+        
+        
+        
+    <tr>
+    <td>
+        <?php
+            
+            
+            echo $aoureltherm;
+        ?>
+    </td>
+    <td>      
+	   <?php
+	        echo $xristinatherm;
+	   ?>
+   	</td>
+    <td>
+	   <?php
+        echo $kevtherm;
+
+	   ?>
+    </td>
+    <td>
+        <?php
+            echo $boutsitherm;
+		?>
+    </td>
+    <td>
+		<?php
+			echo $papadaktherm;
+		?></td>
+		<td><?php
+		 echo $nikostherm;
+		?>
+		</td>
+		<td>
+		<?php
+			echo $kapsalistherm;
+		?>
+		</td>
+		<td>
+		<?php
+			echo $giorgostherm;
+		?>
+		</td>
+        
+        
+        
+        <table id='img'>
+            <a href="meteohistory.php"><br><font size="5"> <center>History</center></font><br></a>
+        
+        <tr id='names'>
+        
+        <th><img src="https://preview.ibb.co/hxMs0H/rain_gauge_filled1600.png" style="width:50px; height:50px"></th>
+        <th><img src="https://image.ibb.co/dj9UVH/radioactive_png_image_64850.png" style="width:50px; height:50"></th>
+        <th><img src="https://image.ibb.co/euyY7x/HAZMAT_Class_7_Radioactive.png"
+        style="width:50px; height:50"></th>
+        <th><img src="https://image.ibb.co/eoCnZc/Anonymous_logo.png" 
+        style="width:50px; height:50"></th>
+        <th><img src="https://preview.ibb.co/ns1wuc/main_800png7.png" style="width:50px; height:50"></th>
+        <th><img src="https://image.ibb.co/bucKEc/thermometer_cold_512.png" style="width:50px; height:50"></th>
+            
+        </tr>
+        <tr>
+        <td>Ιντσες Βροχής</td>
+        <td>Ραδιενέργεια</td>
+        <td>Ακτινοβολία</td>
+        <td>Άγνωστο</td>
+        <td>Μπαταρια</td>
+        <td>(Chill)καφεδακι κ ετσι</td>
+        </tr>
+        <tr>
+        <td>
+            <?php
+                    echo $doulgetherm;
+            ?>
+        </td>    
+        <td>
+            <?php
+                    echo $anastherm;
+            ?>    
+        </td>
+        <td>
+            <?php
+                    echo $dionistherm;
+            ?>
+        </td>
+        <td>
+            <?php
+                    echo $xristtherm;
+            ?>
+        </td>
+        <td>
+            <?php
+                    echo $papadtherm;
+            ?>        
+        </td>
+        <td>
+            <?php
+                    echo $kapsaltherm;
+            ?>        
+        </td>
+        
+
+        </tr>
+        </table>
+        <a href="index.html"><font size="5"> <br><center>BACK</center></font></a>
+</tr>
+</table>
+</body>
+</html>
+
